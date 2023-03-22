5e45dca3b9e8ed479d95a6e49384fd7cb91804c4
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-03-22 21:11:37 +0000

    added method into select heroes

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -15,8 +15,11 @@ def create_connection(db_name, db_user, db_password, db_host = "localhost", db_p
         print(f"The error '{e}' occurred")
     return connection
 
+# create_connection('postgres', 'postgres', 'postgres')    
+
 # using this for majority of time
 # what it does: it uses create connection, accepts query and parameters and sends it off to database
+
 def execute_query(query, params=None):
     connection = create_connection("postgres", "postgres", "postgres")
     cursor = connection.cursor()
@@ -37,7 +40,7 @@ def execute_query(query, params=None):
 # Documentation on this can be found at https://www.psycopg.org/psycopg3/docs/basic/usage.html
 # Examples:
 # def select_all_patients():
-#     query = """
+#     query = 
 #         SELECT * from patients
 
 #     returned_items = execute_query(query).fetchall
@@ -45,12 +48,12 @@ def execute_query(query, params=None):
 #         print(item[1])
 #     return returned_items
 #  def create_new_patient(name, bio):
-#     query = """
+#     query = 
 #         INSERT INTO patients (name, bio)
 #         VALUES (%s, %s)
-#         """
+#         
 #     execute_query(query, (name, bio))
     
-# NOTE: Tuples () with only one argument need to have a trailing comma. 
+# NOTE1: Tuples () with only one argument need to have a trailing comma. 
 # NOTE2: You may remove the print statements in create_connection() and execute_query() to keep from muddying up your project.
     
--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 from database.db_connection import execute_query
 
-execute_query('SELECT * FROM heroes')
+# execute_query('SELECT * FROM heroes')
+
+def select_all_heroes():
+    query = """ 
+        SELECT name 
+        from heroes 
+        
+        """
+    select_all_heroes = execute_query(query).fetchall()
+    for count, value in enumerate(select_all_heroes):
+        print(f'{count +1}: {value[0]}') 
+
+select_all_heroes()
