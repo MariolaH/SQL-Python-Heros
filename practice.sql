53c38c1c23bd87da33b6f4ce33d3b3f8471204ec
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-03-22 23:20:56 +0000

    added stuff

---
+++ practice.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO
+--     heroes (name, about_me, biography)
+-- VALUES
+--     (
+--         'Mrs Muscle',
+--         'Strongest women you have ever met.',
+--         'Mrs Muscle loves to work out 6 - 8 hours a day. For every meal she eats 50 grams of protein and 100 grams of carbs. .'
+--     );
+
+
+SELECT *
+FROM heroes
+JOIN
+    ON
--- src/main.py
+++ src/main.py
@@ -2,6 +2,20 @@ from database.db_connection import execute_query
 
 # execute_query('SELECT * FROM heroes')
 
+def logo():
+    print("""
+
+ ____  ____  ________  _______      ___   ________   ______   
+|_   ||   _||_   __  ||_   __ \   .'   `.|_   __  |.' ____ \  
+  | |__| |    | |_ \_|  | |__) | /  .-.  \ | |_ \_|| (___ \_| 
+  |  __  |    |  _| _   |  __ /  | |   | | |  _| _  _.____`.  
+ _| |  | |_  _| |__/ | _| |  \ \_\  `-'  /_| |__/ || \____) | 
+|____||____||________||____| |___|`.___.'|________| \______.' 
+                                                              
+        HELLO! WELCOME TO THE SUPERHERO DATABASE
+""")
+logo()
+
 def select_all_heroes():
     query = """ 
         SELECT name 
@@ -12,6 +26,18 @@ def select_all_heroes():
     for count, value in enumerate(select_all_heroes):
         print(f'{count +1}: {value[0]}' '\v') 
 
+# def heroes_info():
+#     query = """ 
+#         SELECT name, about_me, biography 
+#         from heroes
+          
+        
+#         """
+#     select_all_heroes = execute_query(query).fetchall()
+#     for count, value in enumerate(select_all_heroes):
+#         print(f'{count +1}: {value[0]}' '\v') 
+
+
 # select_all_heroes()
 
 def begin():
@@ -19,3 +45,25 @@ def begin():
     if start == "":
         select_all_heroes()
 begin()
+
+def options():
+    start = input("Select a Hero by ENTERING in their number to get more info about them: " '\v')
+    if start == "1":
+        print('Chill Women')
+    elif start == "2":
+        print('The Seer')
+    elif start == "3":
+        print('McMuscles')
+    elif start == "4":
+        print('The Hummingbird')
+    elif start == "5":
+        print('Mental Mary')
+    else:
+        print('Lieutenant Lidar')
+options()    
+
+# def Info():
+#     start = input('\v'"Hit ENTER to see some more info about them")
+    
+# Info()
+
